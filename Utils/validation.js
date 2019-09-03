a6712c28faef5db8125984973348f86c951afc9a
umer <umer4167@hotmail.com>
2019-09-03 11:59:09 +0300

    adding register input validation and register resolver

---
+++ Utils/validation.js
@@ -0,0 +1,31 @@
+module.exports = validateRegisterInputs = (
+  username,
+  email,
+  password,
+  confirmPassword
+) => {
+  const errors = {};
+  if (username.trim().length === 0) {
+    errors.username = "Username must not be empty!";
+  }
+  if (email.trim().length === 0) {
+    errors.email = "Email must not be empty!";
+  } else {
+    const emailR = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!email.match(emailR)) {
+      errors.email = "Email must be valid email adress!";
+    }
+  }
+  if (password.trim().length === 0) {
+    errors.password = "Password must not be empty!";
+  } else {
+    if (password !== confirmPassword) {
+      errors.password = "Passwords must match!";
+    }
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1
+  };
+};
--- graphql/Resolvers/user.js
+++ graphql/Resolvers/user.js
@@ -1,18 +1,67 @@
 const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../../dbconfig");
+const { UserInputError } = require("apollo-server");
+const validateRegisterInputs = require("../../Utils/validation");
 
 module.exports = {
   Mutation: {
     register: async (
       _,
-      { registerInput: { username, email, password, confirmPassword } } = args,
+      { registerInput: { username, email, password, confirmPassword } } = args, //multilevel descruturing args
       context,
       info
     ) => {
-      try {
-        //Validate userdata
-        //check for existing data
-        //password hash and generate token
-      } catch (err) {}
+      // try {
+      //Validate userdata
+      const { errors, valid } = validateRegisterInputs(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+      if (!valid) {
+        throw new Error("Inputs are not valid", { errors });
+      }
+      //check for existing data
+      const user = await User.findOne({ username });
+      if (user) {
+        //return error if username is taken
+        throw new UserInputError("Username is taken", {
+          //payload with error object
+          errors: {
+            username: "This username is taken"
+          }
+        });
+      }
+      if (username.trim().length === 0) {
+        throw new Error("username length should not be zero");
+      }
+
+      // try {
+      //hasing password
+      password = await bcrypt.hash(password, 12);
+      const newUser = new User({
+        username,
+        email,
+        password,
+        createdAt: new Date().toISOString()
+      });
+      const res = await newUser.save();
+
+      const token = jwt.sign(
+        {
+          id: res.id,
+          email: res.email,
+          username: res.username
+        },
+        SECRET_KEY,
+        { expiresIn: "1h" }
+      );
+
+      return { ...res._doc, id: res._id, token };
+      // } catch (err) {}
     }
   }
 };
