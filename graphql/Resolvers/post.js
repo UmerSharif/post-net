a9ff5369cbff4e48599ec8ab207ea9a2aeb4ef42
umer <umer4167@hotmail.com>
2019-09-03 19:10:59 +0300

    delete resolver working

--- graphql/Resolvers/post.js
+++ graphql/Resolvers/post.js
@@ -1,5 +1,6 @@
 const Post = require("../../models/Post");
 const isAuth = require("../../Utils/is-auth");
+const { AuthenticationError } = require("apollo-server");
 module.exports = {
   Query: {
     getPosts: async () => {
@@ -43,8 +44,26 @@ module.exports = {
         throw new Error(err);
       }
     },
-    deletePost: async (_, { postId }) => {
-      const post = await Post.findById(postId);
+    deletePost: async (_, { postId }, context) => {
+      const user = isAuth(context);
+
+      if (!user) {
+        throw new Error("Invalid credential for this operation...!");
+      }
+      try {
+        const post = await Post.findById(postId);
+        if (user.username === post.username) {
+          //cheeck if the user is the same who created the post
+          await post.delete();
+          return "Post deleted successfully..!";
+        } else {
+          throw new AuthenticationError(
+            "You are not authorize to delete this post...!"
+          );
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
     }
   }
 };
