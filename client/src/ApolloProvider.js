758b755c66171ab16a298385d32624e07fad65df
umer <umer4167@hotmail.com>
2019-09-22 19:29:43 +0300

    adding heroku adress to frontend

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -8,7 +8,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000"
+  uri: "https://sheltered-shelf-92500.herokuapp.com"
 });
 
 const authLinkHttp = setContext(() => {
